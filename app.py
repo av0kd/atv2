0c9c7a916078abc27127972f699bc8e9fb33b1ee
Darlan Soares <66085720+av0kd@users.noreply.github.com>
2025-01-11 14:47:17 +0000

    feito

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/')
+def products():
+    return render_template('index.html')
+
+@app.route('/produto1')
+def produto1():
+    return render_template('produto1.html')
+
+@app.route('/produto2')
+def produto2():
+    return render_template('produto2.html')
+
+@app.route('/produto3')
+def produto3():
+    return render_template('produto3.html')
+
+@app.route('/produto4')
+def produto4():
+    return render_template('produto4.html')
